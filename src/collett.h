3dd09410879bcf4a3f592b24f26b727c9bde832b
Veronica Berglyd Olsen <1619840+vkbo@users.noreply.github.com>
2021-11-14 20:34:20 +0100

    Add basic build system

---
+++ src/collett.h
@@ -0,0 +1,29 @@
+/*
+Collett – Collett Main Header
+=============================
+
+This file is a part of Collett
+Copyright 2021, Veronica Berglyd Olsen
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <https://www.gnu.org/licenses/>.
+*/
+
+#ifndef COLLETT_H
+#define COLLETT_H
+
+#define COL_VERSION_STR  "0.0.1-alpha0"
+#define COL_VERSION_HEX  0x000001a0
+#define COL_VERSION_DATE "2020-11-14"
+
+#endif // COLLETT_H
---
+++ src/main.cpp
@@ -0,0 +1,36 @@
+/*
+Collett – Main Function
+=======================
+
+This file is a part of Collett
+Copyright 2021, Veronica Berglyd Olsen
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <https://www.gnu.org/licenses/>.
+*/
+
+#include "collett.h"
+
+#include <QApplication>
+
+int main(int argc, char *argv[]) {
+
+    QApplication app(argc, argv);
+
+    QCoreApplication::setOrganizationName("Collett");
+    QCoreApplication::setOrganizationDomain("vkbo.net");
+    QCoreApplication::setApplicationName("Collett");
+    QCoreApplication::setApplicationVersion(COL_VERSION_STR);
+
+    return app.exec();
+}
